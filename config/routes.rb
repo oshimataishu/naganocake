1f7c61b4a92f15575f1d330b07f4f412656599ba
Taishu <oshimataishu@Taishu-MacBook-Air.local>
2023-04-20 09:42:42 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :customers
+  devise_for :admins
+
+  scope module: :public do
+    get 'homes/top'
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
